daeead55c2b019c00bdeec958d648564ba8d0d41
nratheesh <n.ratheesh2010@gmail.com>
2020-05-31 08:04:57 -0400

    rabbitmq configured

---
+++ src/main/java/com/example/messagingrabbitmq/Receiver.java
@@ -0,0 +1,99 @@
+package com.example.messagingrabbitmq;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.log4j.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+//import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.example.messagingrabbitmq.model.Catalina;
+
+@Component
+public class Receiver {
+	
+	@Autowired
+	private Catalina catalina;
+	
+     @Value("${path.file.write}")
+     private String path;
+	
+	//private CountDownLatch latch = new CountDownLatch(1);
+	private static final Logger logger = Logger.getLogger(Receiver.class);
+	/*private static JSONParser parser;
+	
+	public void receiveMessage(String message) {
+		logger.debug("Received : " +message);
+		parser = new JSONParser();
+		try {
+			//JSONObject obj = new JSONObject(message);
+			
+			JSONObject obj = (JSONObject)(parser.parse(message));
+			logger.debug("Message" +obj.toString());
+			String awardNumber = (String) obj.get("award_number");
+			logger.debug("Award Number : " +awardNumber);
+			String jsonmessage = (String) obj.get("message");
+			logger.debug("Message : " +jsonmessage);
+			JSONArray stores = (JSONArray) obj.get("stores");
+			Iterator  i = stores.iterator();
+			while (i.hasNext()) {
+				logger.debug("Store Number : " +i.next());
+			}
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		latch.countDown();
+	}*/
+	
+	@RabbitListener(queues = MessagingRabbitmqApplication.queueName)
+    public void receiveMessage(final Catalina catalina)  {
+   logger.info("Received message as specific class: {}" +catalina.toString());
+   logger.debug("Award Number : " +catalina.getAwardNumber());
+   List<String> stores = catalina.getStores();
+   String fileName = catalina.getAwardNumber();
+   for (String eachStore:stores) {
+	   File file = new File(path+eachStore);
+	   if (!file.exists()) {
+	   file.mkdir();
+	   BufferedWriter bw;
+	try {
+		bw = new BufferedWriter(new FileWriter(path+eachStore+"\\"+catalina.getAwardNumber()+".txt"));
+		bw.write(catalina.getMessage());
+		
+		   bw.flush();
+		   bw.close();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	   
+	   }
+	   //BufferedWriter out = new BufferedWrited(new FileWriter(""))
+   
+   
+       // Logger.info("Received message as specific class: {}", catalina.toString());
+    }
+	
+	
+	
+
+	
+
+}
+}
---
+++ src/main/java/com/example/messagingrabbitmq/model/Catalina.java
@@ -0,0 +1,37 @@
+package com.example.messagingrabbitmq.model;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+@Component
+public class Catalina {
+	
+	private String awardNumber;
+	private List<String> stores;
+	private String message;
+	
+	public String getAwardNumber() {
+		return awardNumber;
+	}
+	public void setAwardNumber(String awardNumber) {
+		this.awardNumber = awardNumber;
+	}
+	public List<String> getStores() {
+		return stores;
+	}
+	public void setStores(List<String> stores) {
+		this.stores = stores;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	@Override
+	public String toString() {
+		return "AwardNumber: " +" "+awardNumber +"Stores: " +stores +" " +"Message: " +message; 
+		
+	}
+
+}
